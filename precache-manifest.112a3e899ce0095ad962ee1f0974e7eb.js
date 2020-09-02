acc261ccdecfc5d5c40295bf8390c8af7a86242a
Anuwatchaturon <anuwat.pu.61@ubu.ac.th>
2020-09-02 11:04:24 +0700

    Updates

---
+++ precache-manifest.112a3e899ce0095ad962ee1f0974e7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754314fda817461cc57e1c5fc972b081",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "7fd4787408e4c0ed53ed",
+    "url": "/tictactoe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "a5de64d64bdf3390f4a3",
+    "url": "/tictactoe/static/js/2.819af240.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/tictactoe/static/js/2.819af240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd4787408e4c0ed53ed",
+    "url": "/tictactoe/static/js/main.c07ff98b.chunk.js"
+  },
+  {
+    "revision": "a943afe6c89606c3dd45",
+    "url": "/tictactoe/static/js/runtime-main.c2a2dfd1.js"
+  }
+]);
